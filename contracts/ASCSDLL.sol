e4663d58a536ba4f30f45adbcc218449e3f6e595
Yorke Rhodes IV <yorke.rhodesiv@consensys.net>
2017-07-19 20:14:15 -0400

    Add insert func

--- contracts/ASCSDLL.sol
+++ contracts/ASCSDLL.sol
@@ -18,4 +18,25 @@ library ASCSDLL {
     function setAttribute(uint curr, bytes32 attributeName, uint attributeValue) {
         store[sha3(msg.sender, curr, attributeName)] = attributeValue;
     }
+
+    function insert(uint prev, uint id, uint[] attributeValues) {
+        require(attributes.length == attributeValues.length);
+
+        uint next = getAttribute(prev, "next");
+
+        // set next node's prev attribute to new node id
+        setAttribute(next, "prev", id);              
+
+        // set prev node's next attribute to new node id
+        setAttribute(prev, "next", id);             
+
+        // make new node point to prev and next
+        setAttribute(id, "prev", prev); 
+        setAttribute(id, "next", next); 
+
+        // set additional attributes of new node
+        for(uint idx = 0; idx < attributes.length; idx++) {
+            setAttribute(curr, attributes[idx], attributeValues[idx]);
+        }
+    }
 }
