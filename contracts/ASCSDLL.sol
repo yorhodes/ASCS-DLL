4b6986514ef728006bde49190a5eb9228465bec5
Yorke Rhodes IV <yorke.rhodesiv@consensys.net>
2017-07-19 20:37:30 -0400

    Fix compilation errors

--- contracts/ASCSDLL.sol
+++ contracts/ASCSDLL.sol
@@ -1,64 +1,66 @@
 pragma solidity^0.4.11;
 
 library ASCSDLL {
-    mapping(bytes32 => uint) store;
+    struct Data {
+        mapping(bytes32 => uint) store;
 
-    bytes32[] attributes;
-    uint sortAttributeIndex;
-
-    function setOptions(bytes32[] _attributes, uint _sortAttributeIndex) {
-        attributes = _attributes;
-        sortAttributeIndex = _sortAttributeIndex;
+        bytes32[] attributes;
+        uint sortAttributeIndex;
     }
 
-    function getAttribute(uint curr, bytes32 attributeName) returns (uint) {
-        return store[sha3(msg.sender, curr, attributeName)];
+    function setOptions(Data storage self, bytes32[] _attributes, uint _sortAttributeIndex) {
+        self.attributes = _attributes;
+        self.sortAttributeIndex = _sortAttributeIndex;
     }
 
-    function setAttribute(uint curr, bytes32 attributeName, uint attributeValue) {
-        store[sha3(msg.sender, curr, attributeName)] = attributeValue;
+    function getAttribute(Data storage self, uint curr, bytes32 attributeName) returns (uint) {
+        return self.store[sha3(msg.sender, curr, attributeName)];
     }
 
-    function insert(uint prev, uint id, uint[] attributeValues) {
-        require(attributes.length == attributeValues.length);
+    function setAttribute(Data storage self, uint curr, bytes32 attributeName, uint attributeValue) {
+        self.store[sha3(msg.sender, curr, attributeName)] = attributeValue;
+    }
 
-        uint next = getAttribute(prev, "next");
+    function insert(Data storage self, uint prev, uint id, uint[] attributeValues) {
+        require(self.attributes.length == attributeValues.length);
+
+        uint next = getAttribute(self, prev, "next");
 
         // set next node's prev attribute to new node id
-        setAttribute(next, "prev", id);              
+        setAttribute(self, next, "prev", id);              
 
         // set prev node's next attribute to new node id
-        setAttribute(prev, "next", id);             
+        setAttribute(self, prev, "next", id);             
 
         // make new node point to prev and next
-        setAttribute(id, "prev", prev); 
-        setAttribute(id, "next", next); 
+        setAttribute(self, id, "prev", prev); 
+        setAttribute(self, id, "next", next); 
 
         // set additional attributes of new node
-        for(uint idx = 0; idx < attributes.length; idx++) {
-            setAttribute(curr, attributes[idx], attributeValues[idx]);
+        for(uint idx = 0; idx < self.attributes.length; idx++) {
+            setAttribute(self, id, self.attributes[idx], attributeValues[idx]);
         }
     }
 
     /// preserves additional data
-    function remove(uint curr) {
-        uint prev = getAttribute(curr, "prev");
-        uint next = getAttribute(curr, "next");
+    function remove(Data storage self, uint curr) {
+        uint prev = getAttribute(self, curr, "prev");
+        uint next = getAttribute(self, curr, "next");
 
-        setAttribute(prev, "next", next);
-        setAttribute(next, "prev", prev);
+        setAttribute(self, prev, "next", next);
+        setAttribute(self, next, "prev", prev);
 
-        setAttribute(curr, "next", curr);
-        setAttribute(curr, "prev", curr);
+        setAttribute(self, curr, "next", curr);
+        setAttribute(self, curr, "prev", curr);
     }
 
     /// destroyes additional data
-    function reset(uint curr) {
-        remove(curr);
+    function reset(Data storage self, uint curr) {
+        remove(self, curr);
 
         // reset additional attributes of node
-        for(uint idx = 0; idx < attributes.length; idx++) {
-            setAttribute(curr, attributes[idx], 0);
+        for(uint idx = 0; idx < self.attributes.length; idx++) {
+            setAttribute(self, curr, self.attributes[idx], 0);
         }
     }
 }
