914ca05bed8242f192a20e828bf97b9a47cdd271
Yorke Rhodes IV <yorke.rhodesiv@consensys.net>
2017-07-19 20:01:21 -0400

    Add config options func

--- contracts/ASCSDLL.sol
+++ contracts/ASCSDLL.sol
@@ -1,5 +1,13 @@
 pragma solidity^0.4.11;
 
 library ASCSDLL {
-	
+    mapping(bytes32 => uint) store;
+
+    bytes32[] attributes;
+    uint sortAttributeIndex;
+
+    function setOptions(bytes32[] _attributes, uint _sortAttributeIndex) {
+        attributes = _attributes;
+        sortAttributeIndex = _sortAttributeIndex;
+    }
 }
