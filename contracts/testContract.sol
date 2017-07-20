1d513feb751f78db675a5254c9167952b110eee4
Cemozerr <cemozer2018@u.northwestern.edu>
2017-07-20 14:56:11 -0400

    Add and link test contract

---
+++ contracts/testContract.sol
@@ -0,0 +1,9 @@
+pragma solidity^0.4.11;
+
+import "./ASCSDLL.sol";
+
+contract  testASCSDLL {
+    using ASCSDLL for ASCSDLL.Data;
+
+    ASCSDLL.Data dll;
+}
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,8 @@
 var ASCSDLL = artifacts.require("./ASCSDLL.sol");
+var testASCSDLL = artifacts.require("./testASCSDLL.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(ASCSDLL);
+    deployer.deploy(ASCSDLL);
+    deployer.link(ASCSDLL, testASCSDLL);
+    deployer.deploy(testASCSDLL);
 };
